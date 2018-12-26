bb7b3006a5f999a278350da57f5c729888f77a24
Claudio Cortese <claudio.cortese@outlook.it>
2018-12-26 22:57:09 +0100

    feat: add clean-webpack-plugin

--- craco.config.js
+++ craco.config.js
@@ -11,6 +11,7 @@ const imageminGiflossy = require('imagemin-giflossy')
 const imageminWebp = require('imagemin-webp')
 const CompressionPlugin = require('compression-webpack-plugin')
 const HtmlCriticalPlugin = require('html-critical-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 const { NODE_ENV } = process.env
 
@@ -45,8 +46,9 @@ module.exports = function() {
     webpack: {
       plugins:
         NODE_ENV === 'development'
-          ? []
+          ? [new CleanWebpackPlugin(['build'])]
           : [
+              new CleanWebpackPlugin(['build']),
               new ImageminPlugin({
                 plugins: [
                   imageminPngquant({
